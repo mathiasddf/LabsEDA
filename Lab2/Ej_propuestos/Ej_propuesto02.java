58fec110f12c47ce36c980d95f91d83d4c4e3694
mathiasddf <mdavilaf@unsa.edu.pe>
2025-05-17 17:39:59 -0500

    Implementacion del metodo rotarizquierdaArray aumentando un parametro mas , para ver cuantas posiciones rotar y uso de main , para comprobar resultados

--- Lab2/Ej_propuestos/Ej_propuesto02.java
+++ Lab2/Ej_propuestos/Ej_propuesto02.java
@@ -1,4 +1,5 @@
 package Lab2.Ej_propuestos;
+import java.util.Scanner;
 
 /*Rotar a la Izquierda, permite ingresar tamaño y captura de valores del arreglo, el método
 rotarIzquierdaArray calcula y muestra el resultado.
@@ -6,5 +7,43 @@ Si d=2
 A=[1 2 3 4 5] -> Ainvertido=[3 4 5 1 2] */
 
 public class Ej_propuesto02 {
-    
+    public static int[] rotarIzquierdaArray(int[] A, int d) {
+        int n = A.length;
+        d = d % n; // normalizar d
+        int[] Ainvertido = new int[n];
+
+        for (int i = 0; i < n - d; i++) {
+            Ainvertido[i] = A[i + d];
+        }
+        for (int i = n - d; i < n; i++) {
+            Ainvertido[i] = A[i - (n - d)];
+        }
+        // Mostrar resultado
+        System.out.print("Arreglo rotado a la izquierda: ");
+        for (int val : Ainvertido) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+        return Ainvertido;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese tamaño del arreglo: ");
+        int n = sc.nextInt();
+
+        int[] A = new int[n];
+        System.out.println("Ingrese los valores del arreglo:");
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        System.out.print("Ingrese número de posiciones a rotar (d): ");
+        int d = sc.nextInt();
+
+        rotarIzquierdaArray(A, d);
+
+        sc.close();
+    }
 }
+
