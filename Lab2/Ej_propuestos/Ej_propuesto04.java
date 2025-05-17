75a937a96d62eac7af7403f2c7369c845af3f9d0
mathiasddf <mdavilaf@unsa.edu.pe>
2025-05-17 18:11:40 -0500

    Implementar trianguloRecursivo2 con un segundo parametro para mejor manejo de espacios y usar main , para probar resultados

--- Lab2/Ej_propuestos/Ej_propuesto04.java
+++ Lab2/Ej_propuestos/Ej_propuesto04.java
@@ -1,5 +1,5 @@
 package Lab2.Ej_propuestos;
-
+import java.util.Scanner;
 /*Triangulo recursivo 2. El método trianguloRecursivo2 calcula y muestra el resultado.
 • Si b = 5
 • Salida:
@@ -9,6 +9,26 @@ package Lab2.Ej_propuestos;
   ****
  *****
 */
+
 public class Ej_propuesto04 {
-    
+    public static void trianguloRecursivo2(int base, int actual) {
+        if (actual > base) return;
+        // Imprimir espacios
+        for (int i = 0; i < base - actual; i++) {
+            System.out.print(" ");
+        }
+        // Imprimir asteriscos
+        for (int i = 0; i < actual; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        trianguloRecursivo2(base, actual + 1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indicar numero de pisos del grafico (base): ");
+        int base = sc.nextInt();
+        trianguloRecursivo2(base, 1);
+        sc.close();
+    }
 }
--- Lab2/Ej_propuestos/Ej_propuesto05.java
+++ Lab2/Ej_propuestos/Ej_propuesto05.java
@@ -1,7 +1,7 @@
 package Lab2.Ej_propuestos;
 
 /*Triangulo recursivo 3. El método trianguloRecursivo3 calcula y muestra el resultado.• Si b = 5
-• Salida: (tipo triangulo △ , aca no se puede ver muy bien  )
+• Salida: (tipo triangulo △ , aca no se puede ver muy bien  4)
     *
     **
    ***
