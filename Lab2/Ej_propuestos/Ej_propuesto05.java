6a3b746bee44b84b4607ce2637f41d4919834b9c
mathiasddf <mdavilaf@unsa.edu.pe>
2025-05-17 18:32:58 -0500

    Implementar trianguloRecursivo3 con un segundo parametro para manejar espacios , y usando main para mostrar resultados

--- Lab2/Ej_propuestos/Ej_propuesto05.java
+++ Lab2/Ej_propuestos/Ej_propuesto05.java
@@ -1,14 +1,39 @@
 package Lab2.Ej_propuestos;
+import java.util.Scanner;
 
 /*Triangulo recursivo 3. El método trianguloRecursivo3 calcula y muestra el resultado.• Si b = 5
 • Salida: (tipo triangulo △ , aca no se puede ver muy bien  4)
-    *
-    **
-   ***
-   ****
-  ***** 
+      *
+     * *
+    * * *
+   * * * *
+  * * * * * 
 */
 
 public class Ej_propuesto05 {
-    
+    public static void trianguloRecursivo3(int totalLineas, int lineaActual) {
+        if (lineaActual > totalLineas) {
+            return; // caso base: ya imprimimos todas las líneas
+        }
+        // imprimir espacios para centrar
+        for (int i = 0; i < totalLineas - lineaActual; i++) {
+            System.out.print(" ");
+        }
+        // imprimir asteriscos con espacios entre ellos
+        for (int i = 0; i < lineaActual; i++) {
+            System.out.print("*");
+            if (i < lineaActual - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        trianguloRecursivo3(totalLineas, lineaActual + 1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de pisos para la pirámide: ");
+        int pisos = sc.nextInt();
+        trianguloRecursivo3(pisos, 1);
+        sc.close();
+    }
 }
