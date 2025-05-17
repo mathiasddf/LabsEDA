700f8ad3fa3724f4e596935d3673847e5c31bc23
mathiasddf <mdavilaf@unsa.edu.pe>
2025-05-17 17:46:53 -0500

    creacion del metodo triangulorecursivo1 y un main para resultados

--- Lab2/Ej_propuestos/Ej_propuesto03.java
+++ Lab2/Ej_propuestos/Ej_propuesto03.java
@@ -1,5 +1,5 @@
 package Lab2.Ej_propuestos;
-
+import java.util.Scanner;
 /*Triangulo recursivo 1. El método trianguloRecursivo1 calcula y muestra el resultado.
 • Si b = 5
 • Salida:
@@ -8,8 +8,24 @@ package Lab2.Ej_propuestos;
  ***
  ****
  ***** 
-*/
+*/  
 
 public class Ej_propuesto03 {
-    
+    public static void trianguloRecursivo1(int base) {
+        if (base > 0) {
+            trianguloRecursivo1(base - 1); // llamada recursiva
+            for (int i = 0; i < base; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indicar numero de pisos del grafico (base): ");
+        int base = sc.nextInt();
+        trianguloRecursivo1(base);
+        sc.close();
+    }
 }
