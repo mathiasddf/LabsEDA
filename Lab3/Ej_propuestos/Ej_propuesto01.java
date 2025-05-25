c26a85e7d2f19fba0897298075b2957a2c47be8c
mathiasddf <mdavilaf@unsa.edu.pe>
2025-05-24 20:31:29 -0500

    subiendo enunciado de ejercicio 01 propuesto

---
+++ Lab3/Ej_propuestos/Ej_propuesto01.java
@@ -0,0 +1,25 @@
+package Lab3.Ej_propuestos;
+
+
+/*
+Listas, Implementar una Lista usando POO con clases y métodos genéricos siguiendo los estándares de
+Java. (Los métodos para una lista)
+- Puede ignorar los siguientes métodos:
+    ✓ hashCode()
+    ✓ iterator()
+    ✓ listIterator()
+    ✓ listIterator(int index)
+    ✓ retainAll(Collection<?> c)
+    ✓ toArray()
+    ✓ toArray(T[] a)
+- Implemente una clase Node<T> donde T es un tipo genérico, esta clase debe contener al menos
+dos propiedades:
+    ✓ T data: la información almacenada en el nodo Node<T> nextNode: una referencia al siguiente
+    nodo
+- Implementar una clase List<T> esta clase debe contener al menos esta propiedad
+    ✓ Node<T> root: la referencia sobre el nodo inicial 
+*/
+
+public class Ej_propuesto01 {
+    
+}
