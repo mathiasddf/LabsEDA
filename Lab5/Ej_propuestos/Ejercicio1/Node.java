4a2b52ece6b9441edf8bd9c331844a88be87e807
mathiasddf <mdavilaf@unsa.edu.pe>
2025-06-07 19:36:33 -0500

    Subiendo clase nodo para el ejercicio 1 propuesto de pila

---
+++ Lab5/Ej_propuestos/Ejercicio1/Node.java
@@ -0,0 +1,22 @@
+package Lab5.Ej_propuestos.Ejercicio1;
+
+public class Node<E> {
+    private E data;
+    private Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+}
