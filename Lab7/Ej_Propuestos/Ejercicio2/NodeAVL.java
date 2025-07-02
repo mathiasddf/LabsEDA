732cc7220aa932b93822ae6e61ed4cd79eb46476
mathiasddf <mdavilaf@unsa.edu.pe>
2025-07-01 22:35:11 -0500

    Subiendo clase nodo para ejericicio 2

---
+++ Lab7/Ej_Propuestos/Ejercicio2/NodeAVL.java
@@ -0,0 +1,12 @@
+package Lab7.Ej_Propuestos.Ejercicio2;
+
+public class NodeAVL<T extends Comparable<? super T>> {
+    T key;
+    NodeAVL<T> left, right;
+    int height;
+
+    public NodeAVL(T key) {
+        this.key   = key;
+        this.height = 1;
+    }
+}
